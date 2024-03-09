1ce49af4f25d35a4402d47f2784f2e3fcff3579d
Pitro979 <pietro.mondino@gmail.com>
2024-03-09 16:51:37 +0000

    Refactoring struttura progetto

--- Server/src/main/java/module-info.java
+++ Server/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module it.project.server {
     requires javafx.controls;
     requires javafx.fxml;
+    requires it.project.lib;
 
     opens it.project.server.controller to javafx.fxml;
     exports it.project.server;
